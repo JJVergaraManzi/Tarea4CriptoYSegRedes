1eda76f46d560969013a83316eeaf6b06bfd6c65
Juan Jose Vergara <40677444+JJVergaraManzi@users.noreply.github.com>
2021-06-20 21:45:42 -0400

    Update hashing.py

--- hashing.py
+++ hashing.py
@@ -1,6 +1,7 @@
-import  bcrypt, glob, cv2
+import  bcrypt as bc
 import socket
-import elgamal, PublicKey, CipherText
+from time import time, sleep
+import  PublicKey, CipherText
 
 path = "C:\Users\juan2\Documents\cripto\Tarea4\Tarea4CriptoYSegRedes\HashyResultados\PotfileHash\hashcat-archivo1Dicc2.potfiles"
 #hashArr1, hashArr2, hashArr3, hashArr4, hashArr5 = []
@@ -25,21 +26,32 @@ t.append(f3.readlines())
 t.append(f4.readlines())
 t.append(f5.readlines())
 passwd = [[],[],[],[],[]]
+filepsswd = open("passwd", "w")
+
+print("Cargando")
+
 
 
 for i in range(0,len(t)):
     for j in range(0, len(t[i])):
+        linea= t[i][j]
         if i== 0:
-            passwd[i].append(line[33:len(line)].strip('\n'))
+            passwd[i].append(linea[33:len(linea)].strip('\n'))
         if i== 1:
-            passwd[i].append(line[50:len(line)].strip('\n'))
+            passwd[i].append(linea[50:len(linea)].strip('\n'))
         if i== 2:
-            passwd[i].append(line[50:len(line)].strip('\n'))
+            passwd[i].append(linea[50:len(linea)].strip('\n'))
         if i== 3:
-            passwd[i].append(line[33:len(line)].strip('\n'))
-
+            passwd[i].append(linea[33:len(linea)].strip('\n'))
+        filepsswd.writelines(passwd[i][len(passwd[i])-1]+"\n")
 #hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+filepsswd.close()
 
+for i in range(0, len(passwd)):
+    fileReport= open("reports/bcryptReport"+str(i), "w")
+    init = time()
+    for j in range(0, len(passwd[i])):
+        hasheado = 
 
 cifAsim= open("CifradoAsimetrico", "r")
 texto = cifAsim.readlines()
